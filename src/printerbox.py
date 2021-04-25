d7e8d07e1ce84547ad011471926520d3784de865
Paul Groskopf <paul@groskopf-embedded.com>
2021-04-25 10:25:52 +0200

    Kan slette udprintet

--- src/printerbox.py
+++ src/printerbox.py
@@ -3,9 +3,9 @@ import subprocess
 import os
 import json
 
-printQueuePdfUrl = 'https://app.conferencecommunicator.com/UserData/PrintQueue/PDF/'
-printQueueUrl = 'https://app.conferencecommunicator.com/plugins/printbox/GetPrintBoxQueue.vbhtml?BoxID='
-
+printQueuePdfUrl =    'https://app.conferencecommunicator.com/UserData/PrintQueue/PDF/'
+printGetQueueUrl =       'https://app.conferencecommunicator.com/plugins/printbox/GetPrintBoxQueue.vbhtml?BoxID='
+printUpdateQueueUrl = 'https://app.conferencecommunicator.com/plugins/printbox/UpdatePrintBoxQueue.vbhtml?ID='
 
 def readLabelFile():
     with open('/labels/navneskilt.txt', 'rt') as navneskilt:
@@ -28,36 +28,41 @@ def downloadPdfFile(fileName):
     return nameTagPdf
 
 def getPrintQueue(boxId):
-    printQueue = requests.get(printQueueUrl + boxId)
+    printQueue = requests.get(printGetQueueUrl + boxId)
     printQueueList = printQueue.text.split('$$')
     return printQueueList
 
 def printFile(fileName, labelName):
-    print(fileName)
+    print("Printing: " + fileName)
     media = 'media=' + labelName
-    printCmd = ['lp', '-d', 'TD4550DNWB', '-h', 'cupsd', '-o', media, fileName]
-    print(printCmd)
-    output = subprocess.run(printCmd, capture_output=False)
+    printCmd = ['lp', '-d', 'TD4550DNWB', '-h', 'printerbox_sortkaffe_cupsd_1', '-o', media, fileName]
+    #print(printCmd)
+    #output = subprocess.run(printCmd, capture_output=False)
     output = subprocess.run(['ls', fileName], capture_output=False)
     return output.returncode
 
-def dequeueFile(fileName):
-    output = subprocess.run(["ls", fileName], capture_output=True)
-    return output.returncode
+def updatePrintQueue(boxId):
+    output = requests.post(printUpdateQueueUrl + boxId)
+    return output.status_code
 
 
 #### Main
 
+print("Starting SortKaffe PrinterBox")
+
 config_file = readConfigFile();
 boxid = config_file['config']['boxid']
 
+print("PrinterBox: " + boxid)
+
+# FIXME retrive the label from the server
 labelName = readLabelFile()
 
-
+# FIXME while for ever
 printQueueList = getPrintQueue(boxid)
 
-print("Hello world")
-print(printQueueList )
+print("Retreiving list:")
+print(printQueueList)
 
 for printQueueElement in printQueueList:
     if(not printQueueElement):
@@ -70,8 +75,7 @@ for printQueueElement in printQueueList:
 
     savePdfFile(nameTagFileName, nameTagPdf.content)
 
-    printFile(nameTagFileName, labelName)
-    #if(printFile(nameTagFileName) == 0):
-     #   os.remove(nameTagFileName)
-      #  dequeueFile(nameTagFileName)
+    if(printFile(nameTagFileName, labelName) == 0):
+        os.remove(nameTagFileName)
+        updatePrintQueue(nameTagFileName)
 
